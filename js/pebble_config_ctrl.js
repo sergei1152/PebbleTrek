6869cb00a0728d882a617503be6eb0571e6e5627
Stephen Zhao <mail@zhaostephen.com>
2015-09-20 06:02:06 -0400

    Update pebble_config_ctrl.js

--- js/pebble_config_ctrl.js
+++ js/pebble_config_ctrl.js
@@ -1,10 +1,12 @@
-var submitButton = $('#save-button');
-submitButton.addEventListener('click', function() {
-	var config = {
-		'emergency_contact_name': $('#name-field').val(),
-		'emergency_contact_phone': $('#phone-field').val(),
-		'countdown_duration': $('#countdown-field').val()
-	}
-	// Return data to watchapp
-	location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(config));
+$(function(){
+	var submitButton = $('#save-button');
+	submitButton.on('click', function() {
+		var config = {
+			'emergency_contact_name': $('#name-field').val(),
+			'emergency_contact_phone': $('#phone-field').val(),
+			'countdown_duration': $('#countdown-field').val()
+		}
+		// Return data to watchapp
+		location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(config));
+	});
 });
